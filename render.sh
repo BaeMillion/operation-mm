095480e1d9031bdee60e4f18c247c5c5091a87e8
dkua <david@kua.io>
2025-01-08 01:53:20 -0500

    Add Friendtaro page
    Sketch courtesy of Miku.

--- render.sh
+++ render.sh
@@ -6,5 +6,6 @@ cp -r public/ dist/public
 minijinja-cli templates/home.html > dist/index.html
 minijinja-cli templates/credits.html > dist/credits.html
 minijinja-cli templates/404.html > dist/404.html
+minijinja-cli templates/friendtaro.html > dist/friendtaro.html
 minijinja-cli templates/messages.html -f json data/messages.json > dist/messages.html
 minijinja-cli templates/timeline.html -f json data/timeline.json > dist/timeline.html
